625e6ff66f6fafe48e574e9224e559a8a6895fe3
Alexander-Guacan <adguacan@espe.edu.ec>
2024-01-23 21:54:18 -0500

    Return students array

--- microservicio1/servicio.js
+++ microservicio1/servicio.js
@@ -6,20 +6,40 @@ const puerto = 3001;
 app.use(cors()); // Habilita CORS
 
 app.get('/', (req, res) => {
-    res.send([
+    const db = [
         {
             firstname: 'john',
-            lastname: 'doe'
+            lastname: 'doe',
+            id: 1752937712,
+            subjects: [1, 3]
         },
         {
             firstname: 'jane',
-            lastname: 'doe'
+            lastname: 'doe',
+            id: 1711270155,
+            subjects: [2, 3]
         },
         {
-            firstname: 'kayne',
-            lastname: 'west'
+            firstname: 'carlos',
+            lastname: 'perez',
+            id: 1717774788,
+            subjects: [2]
         },
-    ]);
+        {
+            firstname: 'mauricio',
+            lastname: 'gomez',
+            id: 1729398483,
+            subjects: [1]
+        },
+        {
+            firstname: 'angel',
+            lastname: 'astudillo',
+            id: 1718283944,
+            subjects: [3]
+        },
+    ]
+
+    res.send(db);
 });
 
 app.listen(puerto, () => {
